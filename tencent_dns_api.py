e4eefd2387295a176ff2b02452e925b1f47f60e6
haiji <wanghj@knownsec.com>
2018-11-16 21:08:17 +0800

    1.更新日志记录函数

--- tencent_dns_api.py
+++ tencent_dns_api.py
@@ -13,11 +13,10 @@ import requests
 import hmac
 import time
 import random
-import commands
 import json
 import re
-from pyquery import PyQuery
 import argparse
+import logging
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
@@ -40,9 +39,18 @@ class IsMail():
             return False
 
 
-def updateLog(log, logfile):
-    log_record = "%s %s" % (time.strftime("%Y-%m-%d %H:%M:%S"), log)
-    commands.getoutput("echo %s >> %s" % (log_record, logfile))
+def appLog(appname, LOGFILE):
+    logger = logging.getLogger(appname)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s %(name)s %(filename)s %(levelname)s %(message)s')
+    formatter.datefmt = '%Y-%m-%d %H:%M:%S'
+
+    file_handler = logging.FileHandler(LOGFILE)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
 
 
 def way_notice(receiver):
@@ -59,7 +67,7 @@ def way_notice(receiver):
     s.quit()
 
 
-def getOwnIp(logfile, RECEIVERS=None):
+def getOwnIp(logger, RECEIVERS=None):
     headers = {"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0"}
     try:
         rep = requests.get("https://haiji.io/get_way_out.php", headers=headers)
@@ -69,7 +77,7 @@ def getOwnIp(logfile, RECEIVERS=None):
             my_ip = json.loads(rep.content)["client_ip"]
             result = {"status": "ok", "ip": my_ip}
     except Exception, e:
-        updateLog(u'当前网络异常，无法获取出口IP, %s' % e, logfile)
+        logger.error(u"当前网络异常，无法获取出口IP，{}".format(e))
         ismail = IsMail()
         rec_failed = []
         if RECEIVERS is not None:
@@ -85,7 +93,7 @@ def getOwnIp(logfile, RECEIVERS=None):
                 else:
                     rec_failed.append(RECEIVERS)
         if len(rec_failed) != 0:
-            updateLog('邮件通知失败，%s' % ';'.join(rec_failed), logfile)
+            logger.error(u'邮件通知失败，%s' % ';'.join(rec_failed))
         result = {"status": "wrong", "msg": "exception"}
     return result
 
@@ -108,7 +116,7 @@ def sign(requestMethod, requestHost, requestPath, params, secretKey):
     return binascii.b2a_base64(hashed.digest())[:-1]
 
 
-def getSubDomains(rootDomain, secret_id, secret_key, logfile):
+def getSubDomains(rootDomain, secret_id, secret_key, logger):
     # 请求参数
     base_arg = {
         "Timestamp": int(time.time()),
@@ -149,13 +157,13 @@ def getSubDomains(rootDomain, secret_id, secret_key, logfile):
         return jsonRet
 
     except Exception, e:
-        updateLog(e, logfile)
+        logger.error(u"{}".format(e))
     finally:
         if httpsConn:
             httpsConn.close()
 
 
-def updateRecord(rootdomain, recordid, host, recordtype, value, secret_id, secret_key, logfile):
+def updateRecord(rootdomain, recordid, host, recordtype, value, secret_id, secret_key, logger):
     # 请求参数
     base_arg = {
         "Timestamp": int(time.time()),
@@ -201,7 +209,7 @@ def updateRecord(rootdomain, recordid, host, recordtype, value, secret_id, secre
         return jsonRet
 
     except Exception, e:
-        updateLog(e, logfile)
+        logger.error(u"{}".format(e))
     finally:
         if httpsConn:
             httpsConn.close()
@@ -209,7 +217,6 @@ def updateRecord(rootdomain, recordid, host, recordtype, value, secret_id, secre
 
 if __name__ == "__main__":
     # LOGFILE = '%s/dns.log' % os.path.abspath(os.path.dirname(__file__))
-    # print(getOwnIp(logfile=LOGFILE))
     # 获取命令行参数
     parsers = argparse.ArgumentParser()
     parsers.add_argument("--secret_id", type=str, help="腾讯云API SECRET ID")
@@ -231,23 +238,25 @@ if __name__ == "__main__":
     if LOGFILE is None:
         LOGFILE = '%s/dns.log' % os.path.abspath(os.path.dirname(__file__))
 
+    logger = appLog("ORAY_INSTEAD", LOGFILE)
+
     dst_hosts = [HOST]
     # 初始化检测 当前出口IP与云端IP异同
-    now_ip = getOwnIp(LOGFILE, RECEIVERS=RECEIVERS)
+    now_ip = getOwnIp(logger, RECEIVERS=RECEIVERS)
     if now_ip["status"] == "ok":
         now_nsip = ''
-        for record in getSubDomains(ROOT_DOMAIN, SecretID, SecretKEY, LOGFILE)["data"]["records"]:
+        for record in getSubDomains(ROOT_DOMAIN, SecretID, SecretKEY, logger)["data"]["records"]:
             for host in dst_hosts:
                 if host == record["name"]:
                     now_nsip = record["value"]
                     if now_ip["ip"] != now_nsip:
                         res = updateRecord(ROOT_DOMAIN, record["id"], host, "A", now_ip["ip"], SecretID, SecretKEY,
-                                           LOGFILE)
+                                           logger)
                         if res["codeDesc"] == "Success":
-                            updateLog("初始化 解析更新成功 新IP为:%s" % (now_ip["ip"]), LOGFILE)
+                            logger.info("初始化 解析更新成功 新IP为:{}".format(now_ip["ip"]))
                             time.sleep(300)
                     else:
-                        updateLog("初始化成功 当前IP为:%s" % (now_ip["ip"]), LOGFILE)
+                        logger.info("初始化成功 当前IP为:{}".format(now_ip["ip"]))
                         time.sleep(300)
     else:
         if now_ip["msg"] == "exception":
@@ -259,10 +268,10 @@ if __name__ == "__main__":
     wait_interval = 10
     exce_interval = 600
     while True:
-        new_ip = getOwnIp(LOGFILE, RECEIVERS=RECEIVERS)
+        new_ip = getOwnIp(logger, RECEIVERS=RECEIVERS)
         if new_ip["status"] == "ok":
             if now_ip["status"] == "ok" and new_ip["ip"] == now_ip["ip"]:
-                updateLog("解析未更新 当前IP为:%s" % (new_ip["ip"]), LOGFILE)
+                logger.info("解析未更新 当前IP为:{}".format(new_ip["ip"]))
                 time.sleep(int(random.random() * 500))
                 pass
             else:
@@ -275,7 +284,7 @@ if __name__ == "__main__":
                             if res["codeDesc"] == "Success":
                                 now_ip["ip"] = new_ip["ip"]
                                 now_ip["status"] = "ok"
-                                updateLog("解析更新成功 新IP为:%s" % (new_ip["ip"]), LOGFILE)
+                                logger.info("解析更新成功 新IP为:{}".format(new_ip["ip"]))
 
         else:
             if new_ip["msg"] == "exception":
