e102ac519ece98a1ceb22cf6da8e04816228c8c8
haiji <teardrop1994@163.com>
2019-02-28 22:43:17 +0800

    更新VENV

--- venv/lib/python2.7/distutils/__init__.py
+++ venv/lib/python2.7/distutils/__init__.py
@@ -4,7 +4,7 @@ import warnings
 import imp
 import opcode # opcode is not a virtualenv module, so we can use it to find the stdlib
               # Important! To work on pypy, this must be a module that resides in the
-              # lib-python/modified-x.y.z directory
+              # cause-python/modified-x.y.z directory
 
 dirname = os.path.dirname
 
