5f262d93e42a38edc9091731b505c6ad8529c7a1
haiji <highgee1994@gmail.com>
2021-03-27 10:50:33 +0000

    例行维护

--- cause/myip.py
+++ cause/myip.py
@@ -8,14 +8,14 @@ from cause.log_handler import own_log
 from cause.config import own_cfg
 from cause.dns_query import DNSQuery
 
-ip_logger = own_log("GET_IP", own_cfg.log_file)
+ip_logger = own_log("GET_IP")
 
 """
 出口IP获取模块
 """
 
 
-def get_ip(ip_host, receivers):
+def get_ip(ip_host):
     result = {"status": "wrong"}
     dst_ip = DNSQuery(ip_host)["ips"][0]
     headers = {
@@ -23,7 +23,7 @@ def get_ip(ip_host, receivers):
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
     }
     try:
-        rep = requests.get("http://{}".format(dst_ip), headers=headers, timeout=5)
+        rep = requests.get("http://{}?type=json".format(dst_ip), headers=headers, timeout=5)
         if rep.status_code != 200:
             ip_logger.error(u"站点{}访问异常，无法获取出口IP，状态码为 {}".format(ip_host, rep.status_code))
             ban_codes = [403, 521, 555]
@@ -35,7 +35,7 @@ def get_ip(ip_host, receivers):
             else:
                 ban_title = u"[未知]出口IP获取异常，状态码为 {}".format(rep.status_code)
             content = "站点 {} 响应出现异常，尽快修复;".format(ip_host)
-            mail_mass(receivers, title=ban_title, content=content)
+            mail_mass(title=ban_title, content=content)
             result["msg"] = "http_status"
         else:
             my_ip = json.loads(rep.content)["client"]
@@ -50,11 +50,11 @@ def get_ip(ip_host, receivers):
     return result
 
 
-def get_local_ip(receivers=None):
+def get_local_ip():
     dms = ["ip.haiji.pro", "ip.haiji.io"]
-    ip_res = get_ip(dms[0], receivers)
+    ip_res = get_ip(dms[0])
     if ip_res["status"] == "wrong":
-        ip_res = get_ip(dms[1], receivers)
+        ip_res = get_ip(dms[1])
         if ip_res["status"] == "wrong":
-            mail_mass(receivers)
+            mail_mass()
     return ip_res
